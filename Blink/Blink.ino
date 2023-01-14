8b8b2ad55c63dfbe049c9ebe572b129b7809dd49
John Woolsey <3237461+woolseyj@users.noreply.github.com>
2023-01-14 10:59:56 -0500

    Updated Blink sketch comments.

--- Blink/Blink.ino
+++ Blink/Blink.ino
@@ -1,9 +1,9 @@
 /*
   Blink
 
-  Based on Blink in 01.Basics Examples.
-
   Turns an LED on for one second, then off for one second, repeatedly.
+
+  Based on the Blink sketch in Main Menu > File > Examples > 01.Basics.
 
   modified 8 May 2014
   by Scott Fitzgerald
@@ -11,24 +11,24 @@
   by Arturo Guadalupi
   modified 8 Sep 2016
   by Colby Newman
-  modified 22 Mar 2018
+  modified 10 Jan 2023
   by John Woolsey
 
-  The example code is in the public domain.
+  This example code is in the public domain.
 
-  http://www.arduino.cc/en/Tutorial/Blink
+  https://www.arduino.cc/en/Tutorial/BuiltInExamples/Blink
 */
 
 // the setup function runs once when you press reset or power the board
 void setup() {
-  // initialize digital pin LED_BUILTIN as an output.
+  // initialize digital pin 2 as an output.
   pinMode(2, OUTPUT);
 }
 
 // the loop function runs over and over again forever
 void loop() {
-  digitalWrite(2, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(1000);             // wait for a second
-  digitalWrite(2, LOW);    // turn the LED off by making the voltage LOW
-  delay(1000);             // wait for a second
+  digitalWrite(2, HIGH);  // turn the LED on (HIGH is the voltage level)
+  delay(1000);            // wait for a second
+  digitalWrite(2, LOW);   // turn the LED off by making the voltage LOW
+  delay(1000);            // wait for a second
 }
