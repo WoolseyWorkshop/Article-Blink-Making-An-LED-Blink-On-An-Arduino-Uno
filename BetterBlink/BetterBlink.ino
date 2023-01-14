cc4384fa0d86d0a3676cc02228e36731ce65ceee
John Woolsey <3237461+woolseyj@users.noreply.github.com>
2023-01-14 14:07:23 -0500

    Updated BetterBlink sketch.
    Changed the blink period and updated comments in the BetterBlink sketch.

--- BetterBlink/BetterBlink.ino
+++ BetterBlink/BetterBlink.ino
@@ -1,16 +1,17 @@
 // BetterBlink - BetterBlink.ino
 //
 // Description:
-// Blinks an LED without using delay().
+// An Arduino sketch that blinks an LED without using the delay() function.
 //
 // Circuit:
-// - A red LED is connected to pin D2.
+// - An LED is connected to pin D2.
 //
+// Created by John Woolsey on 10/22/2020.
+// Modified by John Woolsey on 01/09/2023.
 // Copyright (c) 2020 Woolsey Workshop.  All rights reserved.
 
-
 const uint8_t RedLED = 2;                // reference digital pin 2 as RedLED
-const unsigned long BlinkPeriod = 1000;  // blinking period in milliseconds
+const unsigned long BlinkPeriod = 2000;  // blinking period in milliseconds
 
 void setup() {
    pinMode(RedLED, OUTPUT);  // set RedLED pin as an output
@@ -19,7 +20,7 @@ void setup() {
 void loop() {
    static unsigned long previousBlinkTime = 0;              // capture initial previous time
    unsigned long currentTime = millis();                    // capture current time
-   if (currentTime - previousBlinkTime >= BlinkPeriod/2) {  // run every half blinking period (500 ms)
+   if (currentTime - previousBlinkTime >= BlinkPeriod/2) {  // run every half blinking period (1 s)
       digitalWrite(RedLED, !digitalRead(RedLED));           // toggle on/off LED
       previousBlinkTime = currentTime;                      // save previous time
    }
